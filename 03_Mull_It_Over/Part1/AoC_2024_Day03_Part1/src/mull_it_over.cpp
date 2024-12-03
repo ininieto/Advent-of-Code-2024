0bcdd4ff2980ba866e1da0d89b01372fad27954b
ininieto <ininieto8@gmail.com>
2024-12-03 19:21:59 +0100

    Solved day 3 part 1

---
+++ 03_Mull_It_Over/Part1/AoC_2024_Day03_Part1/src/mull_it_over.cpp
@@ -0,0 +1,81 @@
+/*
+	Advent of Code 2024 - 03.12.2024
+
+	Link to the problem: https://adventofcode.com/2024/day/3
+
+	Author of the solution: Inigo Nieto Cuadrado
+
+*/
+
+#include "mull_it_over.h"
+
+#include <iostream>
+
+#include "common.h"
+
+int main() {
+
+	// Read the example and the input data
+	std::string example = Common::readInputText("../../resources/example.txt");
+	std::string input = Common::readInputText("../../resources/input.txt");
+
+	// Work with example
+	// input = example;
+
+	long int result = 0;
+
+	// Read character by character the string
+	for (size_t i = 0; i < input.length(); i++) {
+
+		// Try to read "mul("
+		if (input[i] == 'm' && input.substr(i, 4) == "mul(") {
+
+			size_t pos = i + 4;	// Start reading after "mul("
+
+			// Start reading the first number
+			std::string strFirstNum, strSecondNum;
+			int firstNum = -1, secondNum = -1;
+
+			for (int j = pos; j < input.length(); j++) {
+				if (isdigit(input[j]) && strFirstNum.length() < 3) {
+					strFirstNum.push_back(input[j]);
+					pos++;
+				}
+				else if (input[j] == ',' && !strFirstNum.empty()) { // If we find a comma and already found some numbers
+					firstNum = stoi(strFirstNum);
+					pos++;
+					break;
+				}
+				else
+					break;
+			}
+
+			// If a number was found, look for the second one
+			if (firstNum > 0) {
+				for (int j = pos; j < input.length(); j++) {   // We need to read after the comma
+
+					if (isdigit(input[j]) && strSecondNum.length() < 3) {
+						strSecondNum.push_back(input[j]);
+						pos++;
+					}
+					else if (input[j] == ')' && !strSecondNum.empty()) { // If we find a closing bracket and already found some numbers
+						secondNum = stoi(strSecondNum);
+						pos++;
+						break;
+					}
+					else
+						break;
+				}
+
+				// If we found two numbers, we can now multiply them
+				if (secondNum > 0)
+					result += firstNum * secondNum;
+			}
+		}
+	}
+
+	// Print the result
+	std::cout << result;
+
+	return 0;
+}
