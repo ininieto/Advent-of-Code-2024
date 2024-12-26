d85e17c058c53d5c155e1939add3c0ffcc9421cc
ininieto <ininieto8@gmail.com>
2024-12-26 22:43:59 +0100

    Solved day 13 part 1

---
+++ 13_Claw_Contraption/Part1/AoC_2024_Day13_Part1/src/claw_contraption.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include <string>
+
+void parseInput(const std::string& line, int& x, int& y);
+bool solveEquation(const int a, const int b, const int c, const int d, const int e, const int f, double& solX, double& solY);
