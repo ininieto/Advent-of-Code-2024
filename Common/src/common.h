d506a21ba23036a5c13d8f2d4ccf6e514213ee9a
ininieto <ininieto8@gmail.com>
2025-04-16 17:01:42 +0200

    Added struct Tile to Common

--- Common/src/common.h
+++ Common/src/common.h
@@ -23,6 +23,25 @@ struct Coords {
 	}
 };
 
+struct Tile {
+
+	Coords pos;
+	int dir;
+	uint64_t dist;
+
+	// Default constructor
+	Tile() : pos(Coords(-1, -1)), dir(-1), dist(INT_MAX) {}
+
+	// Param constructor
+	Tile(Coords pos, int dir, uint64_t dist) : pos(pos), dir(dir), dist(dist) {}
+
+	// Comparator to create the priority queue
+	bool operator<(const Tile& other) const {
+		return dist > other.dist;	// Smallest dist has the priority
+	}
+};
+
+
 namespace Common {
 
 	std::string readInputText(const std::string &inputText);
